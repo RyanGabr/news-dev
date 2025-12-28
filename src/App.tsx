40b258a2858eacd8e6783fc739621e27dc93c2d7
Ryan Gabriel <ryanalexandregabriel@gmail.com>
2025-12-28 18:10:48 -0300

    feat: add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/query-client";
+import { ThemeProvider } from "./components/ui/theme-provider";
 
 export function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
