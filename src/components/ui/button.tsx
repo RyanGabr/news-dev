d14521ca5046526769f3b0365bd4137c22acfc79
Ryan Gabriel <ryanalexandregabriel@gmail.com>
2026-01-05 08:51:14 -0300

    style: improve login page UI

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,20 +3,30 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { twMerge } from "tailwind-merge";
 
 const buttonVariants = cva(
-  "cursor-pointer flex items-center justify-center gap-1 rounded-full font-semibold transition disabled:opacity-50 disabled:cursor-default",
+  "cursor-pointer flex items-center justify-center gap-1 font-medium transition disabled:opacity-50 disabled:cursor-default",
   {
     variants: {
       variant: {
         default: "bg-foreground text-background hover:opacity-90",
-        secondary: "bg-foreground/7 text-foreground hover:bg-foreground/10",
+        secondary:
+          "bg-secondary-foreground text-foreground hover:bg-foreground/10",
         outline: "bg-transparent border border-border",
+        ghost: "hover:bg-secondary-foreground font-normal",
+        link: "hover:underline font-normal text-muted-foreground",
       },
       size: {
-        default: "text-base px-4.5 py-2",
+        default: "text-base px-4 py-2",
+        sm: "text-sm px-3 py-2",
+        link: "p-0 text-sm",
+      },
+      rounded: {
+        default: "rounded-md",
+        full: "rounded-full",
       },
     },
     defaultVariants: {
       variant: "default",
+      rounded: "default",
       size: "default",
     },
   },
@@ -28,10 +38,12 @@ export interface ButtonProps
     VariantProps<typeof buttonVariants> {}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
+  ({ className, variant, size, rounded, ...props }, ref) => {
     return (
       <button
-        className={twMerge(buttonVariants({ variant, size, className }))}
+        className={twMerge(
+          buttonVariants({ variant, size, rounded, className }),
+        )}
         ref={ref}
         {...props}
       />
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -7,6 +7,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { HugeiconsIcon } from "@hugeicons/react";
 import { ViewIcon, ViewOffIcon } from "@hugeicons/core-free-icons";
+import { Label } from "@/components/ui/label";
 
 export function Login() {
   const [passwordInputType, setPasswordInputType] =
@@ -38,89 +39,125 @@ export function Login() {
   }
 
   return (
-    <main className="w-full h-screen">
+    <main className="w-full h-screen bg-secondary-foreground">
       <form
         onSubmit={handleSubmit(handleLogin)}
-        className="absolute top-1/2 left-1/2 -translate-1/2 w-96 flex flex-col gap-5 items-center justify-center"
+        className="absolute top-1/2 left-1/2 -translate-1/2 w-[calc(100%-2rem)] max-w-2xl flex flex-col lg:flex-row bg-background rounded-2xl divide-y lg:divide-x lg:divide-y-0"
       >
-        <div>
-          <h2 className="font-bold text-2xl">Entrar</h2>
-        </div>
-
-        <div className="space-y-5 w-full">
-          <div className="flex flex-col gap-1.5">
-            <input
-              {...register("email")}
-              aria-invalid={!!errors.email}
-              type="email"
-              placeholder="Insira seu endereço de email"
-              className="bg-foreground/5 py-3 px-4 rounded-md text-[15px] outline-0"
-            />
-            {errors.email && (
-              <span className="text-sm text-red-500">
-                {errors.email.message}
-              </span>
-            )}
-          </div>
-
-          <div className="flex flex-col gap-1.5">
-            <div className="relative w-full">
-              <input
-                {...register("password")}
-                aria-invalid={!!errors.password}
-                type={passwordInputType}
-                placeholder="Senha"
-                className="bg-foreground/5 py-3 px-4 rounded-md text-[15px] outline-0 w-full"
+        <div className="p-6 flex flex-col justify-between w-full lg:w-1/2">
+          <div className="space-y-3">
+            <div className="flex items-center gap-2">
+              <img
+                src="https://www.notion.com/front-static/favicon.ico"
+                alt=""
+                className="w-6"
               />
-              <Button
-                type="button"
-                onClick={toggleShowPassword}
-                aria-label={
-                  passwordInputType === "password"
-                    ? "Mostrar senha"
-                    : "Ocultar senha"
-                }
-                className="absolute top-1/2 -translate-y-1/2 right-3 p-1 bg-transparent text-foreground"
-              >
-                <HugeiconsIcon
-                  icon={
-                    passwordInputType === "password" ? ViewIcon : ViewOffIcon
-                  }
-                  size={22}
-                  strokeWidth={2}
-                />
-              </Button>
+
+              <h2 className="font-semibold text-2xl tracking-tight">
+                Entrar no news.dev
+              </h2>
             </div>
 
-            {errors.password && (
-              <span className="text-sm text-red-500">
-                {errors.password.message}
-              </span>
-            )}
-          </div>
-        </div>
-
-        <div className="w-full">
-          <Button type="submit" disabled={isPending} className="w-full py-3">
-            {isPending ? "Entrando..." : "Avançar"}
-          </Button>
-        </div>
-
-        {apiError && (
-          <div className="text-center">
-            <p className="text-sm text-red-500">
-              {apiError.message === "WRONG_CREDENTIALS"
-                ? "Email ou senha incorretos. Tente novamente"
-                : "Erro ao realizar o login. Tente novamente."}
+            <p className="text-muted-foreground text-sm">
+              Uma plataforma de publicação de conteúdo, fique por dentro de tudo
+              que está acontecendo.
             </p>
           </div>
-        )}
 
-        <div className="w-full flex items-center justify-center gap-1">
-          <p>Não possui uma conta?</p>
-          <Link to="/signup" className="text-blue-500 hover:underline">
-            Inscreva-se
-          </Link>
+          <div>
+            <div className="w-full flex items-center gap-1.5 text-sm text-muted-foreground">
+              <p>Não possui uma conta?</p>
+              <Link
+                to="/signup"
+                className="text-foreground hover:opacity-90 transition"
+              >
+                Inscreva-se
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-6 w-full lg:w-1/2 space-y-4">
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-1.5">
+              <Label>Email</Label>
+
+              <input
+                {...register("email")}
+                aria-invalid={!!errors.email}
+                type="email"
+                placeholder="ryan@mail.com"
+                className="py-2.5 text-sm outline-0 border-b focus:border-foreground"
+              />
+              {errors.email && (
+                <span className="text-xs text-red-500">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
+
+            <div className="flex flex-col gap-1.5">
+              <Label>Senha</Label>
+
+              <div className="relative w-full">
+                <input
+                  {...register("password")}
+                  aria-invalid={!!errors.password}
+                  type={passwordInputType}
+                  placeholder="******"
+                  className="py-2.5 text-sm outline-0 border-b w-full focus:border-foreground"
+                />
+                <Button
+                  type="button"
+                  size="sm"
+                  variant="ghost"
+                  onClick={toggleShowPassword}
+                  aria-label={
+                    passwordInputType === "password"
+                      ? "Mostrar senha"
+                      : "Ocultar senha"
+                  }
+                  className="absolute top-1/2 -translate-y-1/2 right-0 p-1"
+                >
+                  <HugeiconsIcon
+                    icon={
+                      passwordInputType === "password" ? ViewIcon : ViewOffIcon
+                    }
+                    size={18}
+                    strokeWidth={2}
+                  />
+                </Button>
+              </div>
+
+              {errors.password && (
+                <span className="text-xs text-red-500">
+                  {errors.password.message}
+                </span>
+              )}
+            </div>
+
+            <div className="w-full">
+              <Button
+                type="submit"
+                size="sm"
+                rounded="full"
+                disabled={isPending}
+                className="w-full py-2.5"
+              >
+                {isPending ? "Entrando..." : "Continuar"}
+              </Button>
+            </div>
+          </div>
+
+          {apiError && (
+            <div className="text-center">
+              <p className="text-xs text-red-500">
+                {apiError.message === "WRONG_CREDENTIALS"
+                  ? "Email ou senha incorretos. Tente novamente"
+                  : "Erro ao realizar o login. Tente novamente."}
+              </p>
+            </div>
+          )}
         </div>
       </form>
     </main>
