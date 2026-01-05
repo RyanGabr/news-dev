0ae3ad55d68baddb6b10019a90d89978b1ea7bcd
Ryan Gabriel <ryanalexandregabriel@gmail.com>
2026-01-05 08:56:01 -0300

    style: decrease the stroke width of the 'show password' button icon on the login page.

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -124,7 +124,6 @@ export function Login() {
                       passwordInputType === "password" ? ViewIcon : ViewOffIcon
                     }
                     size={18}
-                    strokeWidth={2}
                   />
                 </Button>
               </div>
