441596b5ce1ed823da555cd7cbfe80a53febb990
Ryan Gabriel <ryanalexandregabriel@gmail.com>
2026-01-07 10:03:26 -0300

    style: change post page width

--- src/pages/app/post.tsx
+++ src/pages/app/post.tsx
@@ -3,7 +3,7 @@ import { PostHeader } from "@/components/post/post-header";
 
 export function Post() {
   return (
-    <div className="max-w-3xl mx-auto px-6 lg:px-0 py-10 space-y-10">
+    <div className="max-w-2xl mx-auto px-6 lg:px-0 py-10 space-y-10">
       <div className="space-y-8 md:space-y-14">
         <PostHeader />
         <PostContent />
