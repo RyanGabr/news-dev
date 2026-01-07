398664042a2911ba25829a9657aae74a82dcba6a
Ryan Gabriel <ryanalexandregabriel@gmail.com>
2026-01-07 20:23:17 -0300

    refactor: separate delete post dialog into a component

---
+++ src/components/post/delete-post.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { useDeletePost } from "@/hooks/use-delete-post";
+import { useNavigate, useParams } from "react-router-dom";
+import { DropdownMenuItem } from "../ui/dropdown-menu";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { Delete02Icon } from "@hugeicons/core-free-icons";
+import { Button } from "../ui/button";
+
+export function DeletePost() {
+  const { id } = useParams();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const { mutateAsync, isPending } = useDeletePost();
+  const navigate = useNavigate();
+
+  async function deletePost() {
+    if (id) {
+      await mutateAsync(id);
+      navigate("/");
+    }
+  }
+
+  return (
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger asChild>
+        <DropdownMenuItem
+          variant="destructive"
+          onClick={(e) => {
+            e.preventDefault();
+            setIsDialogOpen(true);
+          }}
+        >
+          Deletar
+          <HugeiconsIcon icon={Delete02Icon} strokeWidth={2} />
+        </DropdownMenuItem>
+      </DialogTrigger>
+      <DialogContent className="w-sm">
+        <DialogTitle>Deletar postagem</DialogTitle>
+        <DialogDescription>
+          Você tem certeza dessa ação? A postagem será deletada permanentemente.
+        </DialogDescription>
+
+        <DialogFooter className="flex-row sm:justify-normal">
+          <DialogClose asChild>
+            <Button
+              variant="secondary"
+              size="sm"
+              rounded="full"
+              className="w-full"
+            >
+              Cancelar
+            </Button>
+          </DialogClose>
+          <Button
+            onClick={deletePost}
+            disabled={isPending}
+            size="sm"
+            rounded="full"
+            className="w-full"
+          >
+            Deletar
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/post/post-options.tsx
+++ src/components/post/post-options.tsx
@@ -1,11 +1,12 @@
+import { useGetPost } from "@/hooks/use-get-post";
 import {
-  Delete02Icon,
   Edit03Icon,
   MoreHorizontalIcon,
   Share08Icon,
 } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useUser } from "@supabase/auth-helpers-react";
+import { useParams } from "react-router-dom";
 import { Button } from "../ui/button";
 import {
   DropdownMenu,
@@ -13,100 +14,34 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
-import { useGetPost } from "@/hooks/use-get-post";
-import { useUser } from "@supabase/auth-helpers-react";
-import { useDeletePost } from "@/hooks/use-delete-post";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogTitle,
-  DialogTrigger,
-} from "../ui/dialog";
-import { useState } from "react";
+import { DeletePost } from "./delete-post";
 
 export function PostOptions() {
   const { id } = useParams();
   const { data: post } = useGetPost(id ?? "");
   const user = useUser();
 
-  const { mutateAsync, isPending } = useDeletePost();
-  const navigate = useNavigate();
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-
-  async function deletePost() {
-    if (id) {
-      await mutateAsync(id);
-      navigate("/");
-    }
-  }
-
   return (
-    <div className="flex items-center gap-2">
-      {post.author_id === user?.id && (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="secondary" size="sm" rounded="full">
-              <HugeiconsIcon icon={MoreHorizontalIcon} size={20} />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="start" className="w-52 p-1.5">
-            <DropdownMenuItem>
-              Compartilhar
-              <HugeiconsIcon icon={Share08Icon} strokeWidth={2} />
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Editar
-              <HugeiconsIcon icon={Edit03Icon} strokeWidth={2} />
-            </DropdownMenuItem>
-            <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-              <DialogTrigger asChild>
-                <DropdownMenuItem
-                  variant="destructive"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setIsDialogOpen(true);
-                  }}
-                >
-                  Deletar
-                  <HugeiconsIcon icon={Delete02Icon} strokeWidth={2} />
-                </DropdownMenuItem>
-              </DialogTrigger>
-              <DialogContent className="w-sm">
-                <DialogTitle>Deletar postagem</DialogTitle>
-                <DialogDescription>
-                  Você tem certeza dessa ação? A postagem será deletada
-                  permanentemente.
-                </DialogDescription>
+    post.author_id === user?.id && (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="secondary" size="sm" rounded="full">
+            <HugeiconsIcon icon={MoreHorizontalIcon} size={20} />
+          </Button>
+        </DropdownMenuTrigger>
 
-                <DialogFooter className="flex-row sm:justify-normal">
-                  <DialogClose asChild>
-                    <Button
-                      variant="secondary"
-                      size="sm"
-                      rounded="full"
-                      className="w-full"
-                    >
-                      Cancelar
-                    </Button>
-                  </DialogClose>
-                  <Button
-                    onClick={deletePost}
-                    disabled={isPending}
-                    size="sm"
-                    rounded="full"
-                    className="w-full"
-                  >
-                    Deletar
-                  </Button>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )}
-    </div>
+        <DropdownMenuContent align="start" className="w-52 p-1.5">
+          <DropdownMenuItem>
+            Compartilhar
+            <HugeiconsIcon icon={Share08Icon} strokeWidth={2} />
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Editar
+            <HugeiconsIcon icon={Edit03Icon} strokeWidth={2} />
+          </DropdownMenuItem>
+          <DeletePost />
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
   );
 }
