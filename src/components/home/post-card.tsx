2f42ee1d76b9562cfdff86ef0610117031bc0071
Ryan Gabriel <ryanalexandregabriel@gmail.com>
2026-01-07 10:25:06 -0300

    refactor: remove comments from post-card.tsx code

--- src/components/home/post-card.tsx
+++ src/components/home/post-card.tsx
@@ -7,18 +7,6 @@ interface PostCardProps {
 }
 
 export function PostCard({ post }: PostCardProps) {
-  // const postCreatedAt = new Date(post.created_at || "");
-
-  // const dateOptions: Intl.DateTimeFormatOptions = {
-  //   month: "short",
-  //   day: "numeric",
-  // };
-
-  // const postFormattedDate = new Intl.DateTimeFormat(
-  //   "pt-BR",
-  //   dateOptions,
-  // ).format(postCreatedAt);
-
   return (
     <Link
       to={`/post/${post.id}`}
@@ -41,19 +29,6 @@ export function PostCard({ post }: PostCardProps) {
           <MarkdownCleaner markdown={post.content} />
         </div>
       </div>
-
-      {/*<div className="flex items-center gap-2">
-        <img
-          src="https://pbs.twimg.com/profile_images/1999199376619581440/8W7FN5gc_400x400.jpg"
-          alt=""
-          className="min-w-6 max-w-6 rounded-full"
-        />
-        <div>
-          <span className="text-sm text-muted-foreground">
-            {post.profiles.username}
-          </span>
-        </div>
-      </div>*/}
     </Link>
   );
 }
