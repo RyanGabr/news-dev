1b53d33cf73f67621b6a41d374a12350980e1c08
Ryan Gabriel <ryanalexandregabriel@gmail.com>
2026-01-09 20:30:33 -0300

    style: increase homepage padding

--- src/pages/app/home.tsx
+++ src/pages/app/home.tsx
@@ -5,7 +5,7 @@ import { Suspense } from "react";
 
 export function Home() {
   return (
-    <div className="px-4 w-full xl:px-0 xl:w-3/4 2xl:w-1/2 mx-auto py-10 space-y-5">
+    <div className="px-6 w-full xl:px-0 xl:w-3/4 2xl:w-1/2 mx-auto py-10 space-y-5">
       <Suspense fallback={<Loading />}>
         <Categories />
         <PostList />
