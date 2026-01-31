c539ed9ee9e9eb2d93496bffb916b0a9b386e317
Ryan Gabriel <ryanalexandregabriel@gmail.com>
2026-01-31 17:53:40 -0300

    style: improve home header UI

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,12 +1,23 @@
+import { Button } from "../ui/button";
+
 export function Header() {
   return (
-    <div className="flex flex-col gap-4 mb-14">
-      <h1 className="text-3xl font-semibold">Explorar</h1>
+    <div className="flex flex-col items-center justify-center gap-6 md:pt-10">
+      <h1 className="hidden md:block text-5xl font-medium tracking-tight text-center">
+        Leia, escreva e compartilhe <br /> com a comunidade.
+      </h1>
 
-      <p className="text-muted-foreground">
-        Explore eventos populares perto de você, navegue por categoria ou
-        confira alguns dos ótimos calendários da comunidade.
+      <h1 className="md:hidden text-4xl font-medium">Explorar</h1>
+
+      <p className="text-muted-foreground text-lg font-medium hidden md:block text-center">
+        Uma comunidade feita para compartilhar conhecimentos, debater <br />{" "}
+        ideias e encontrar relevância em assuntos variados.
       </p>
+
+      <div className="items-center gap-3 hidden md:flex">
+        <Button>Start now</Button>
+        <Button variant="outline">Estrela no GitHub</Button>
+      </div>
     </div>
   );
 }
