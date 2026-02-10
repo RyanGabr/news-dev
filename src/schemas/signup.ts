dc971512911ffc9e648a97d506a2a3ea092e15bf
Ryan Gabriel <ryanalexandregabriel@gmail.com>
2026-02-10 09:27:52 -0300

    refactor: ensure usernames are always processed as lowercase

--- src/schemas/signup.ts
+++ src/schemas/signup.ts
@@ -1,11 +1,13 @@
 import { z } from "zod";
 
 export const signupSchema = z.object({
+  display_name: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
   username: z
     .string()
     .trim()
     .min(3, "O nome de usuário deve ter pelo menos 3 caracteres")
-    .regex(/^\S+$/, "Não pode conter espaços"),
+    .regex(/^\S+$/, "Não pode conter espaços")
+    .transform((val) => val.toLowerCase()),
   email: z.string().email("E-mail inválido"),
   password: z.string().min(6, "A senha precisa ter pelo menos 6 caracteres"),
 });
